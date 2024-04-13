999e0f49981f8722bedad9a8acb69f138ef0d366
Oleg <kit.oleg167@gmail.com>
2024-04-13 13:14:37 +0300

    Add project files.

---
+++ CodeCone.Ducks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CodeCone.Ducks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
