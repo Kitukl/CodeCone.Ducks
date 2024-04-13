f48f8e49f0007fb0048c6e62344f4a4389721eb6
Oleg <kit.oleg167@gmail.com>
2024-04-13 21:50:22 +0300

    +migration

--- CodeCone.Ducks/Migrations/20240413184757_MyMigration.Designer.cs
+++ CodeCone.Ducks/Migrations/20240413184757_MyMigration.Designer.cs
@@ -4,6 +4,7 @@ using CodeCone.Ducks.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CodeCone.Ducks.Migrations
 {
     [DbContext(typeof(CodeConeDucksContext))]
-    partial class CodeConeDucksContextModelSnapshot : ModelSnapshot
+    [Migration("20240413184757_MyMigration")]
+    partial class MyMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CodeCone.Ducks/Migrations/20240413184757_MyMigration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CodeCone.Ducks.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isVolontier",
+                table: "User",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isVolontier",
+                table: "User");
+        }
+    }
+}
